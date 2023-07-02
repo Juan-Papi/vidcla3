678cf8b305c4e560bfefcd583246d8bbdfa5f4ad
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2023-07-02 19:25:55 -0400

    filtrar bitacora por email, name, fecha(desde,hasta)

--- app/Http/Livewire/BitacoraComponent.php
+++ app/Http/Livewire/BitacoraComponent.php
@@ -5,17 +5,47 @@ namespace App\Http\Livewire;
 use App\Models\Bitacora;
 use Livewire\Component;
 use Livewire\WithPagination;
+
 class BitacoraComponent extends Component
 {
-    use WithPagination;
-    protected $paginationTheme = "bootstrap";
+  use WithPagination;
+  protected $paginationTheme = "bootstrap";
 
-    public function render()
-    {
-      /*Solo asegúrate de que estás cargando la relación user cuando obtienes las bitácoras, para evitar el problema de carga diferida (N+1). Es decir, cuando obtienes las bitácoras, debes hacer algo como esto:*/
-      //$bitacoras = Bitacora::with('user')->paginate(10);
+  public $search;
+  public $fecha_desde;
+  public $fecha_hasta;
 
-        $bitacoras = Bitacora::orderBy('id', 'DESC')->paginate(10);
-        return view('livewire.bitacora-component', compact('bitacoras'));
+  public function updatingSearch()
+  {
+    $this->resetPage();
+  }
+
+  public function render()
+  {
+    /*Solo asegúrate de que estás cargando la relación user cuando obtienes las bitácoras, para evitar el problema de carga diferida (N+1). Es decir, cuando obtienes las bitácoras, debes hacer algo como esto:*/
+    //$bitacoras = Bitacora::with('user')->paginate(10);
+
+    $bitacoras = Bitacora::query();
+
+    // Search for user name or email.
+    if ($this->search) {
+      $bitacoras = $bitacoras->whereHas('user', function ($q) {
+        $q->where('name', 'like', '%' . $this->search . '%')
+          ->orWhere('email', 'like', '%' . $this->search . '%');
+      });
     }
+
+    // Filter date range
+    if ($this->fecha_desde) {
+      $bitacoras = $bitacoras->where('fecha', '>=', $this->fecha_desde);
+    }
+
+    if ($this->fecha_hasta) {
+      $bitacoras = $bitacoras->where('fecha', '<=', $this->fecha_hasta);
+    }
+
+    $bitacoras = $bitacoras->with('user')->orderBy('id', 'DESC')->paginate(10);
+
+    return view('livewire.bitacora-component', compact('bitacoras'));
+  }
 }
--- resources/views/livewire/bitacora-component.blade.php
+++ resources/views/livewire/bitacora-component.blade.php
@@ -1,20 +1,40 @@
 <div>
     <div class="card">
+        <div class="card-header">
+            <div class="form-group row align-items-center">
+                <label for="search" class="col-form-label col-md-3">Buscar por usuario o email:</label>
+                <div class="col-md-9">
+                    <input type="text" id="search" class="form-control" placeholder="Buscar..." wire:model="search">
+                </div>
+            </div>
+            <div class="form-group row align-items-center">
+                <label for="fecha_desde" class="col-form-label col-md-1 mr-2">Desde:</label>
+                <div class="col-md-4">
+                    <input type="date" id="fecha_desde" class="form-control" placeholder="Desde" wire:model="fecha_desde">
+                </div>
+                <label for="fecha_hasta" class="col-form-label col-md-1 ml-5 mr-2">Hasta:</label>
+                <div class="col-md-4">
+                    <input type="date" id="fecha_hasta" class="form-control" placeholder="Hasta" wire:model="fecha_hasta">
+                </div>
+            </div>
+        </div>        
+        
+        
         @if ($bitacoras->count())
             <div class="card-body">
                 <table class="table table-striped">
                     <thead>
-                        <tr>               
+                        <tr>
                             <th>ID</th>
                             <th>USER_ID</th>
                             <th>USER_NAME</th>
                             <th>ACCION</th>
-                            <th>FECHA_HORA</th>            
+                            <th>FECHA_HORA</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($bitacoras as $bitacora)
-                            <tr>               
+                            <tr>
                                 <td>
                                     {{ $bitacora->id }}
                                 </td>
@@ -28,8 +48,8 @@
                                     {{ $bitacora->accion }}
                                 </td>
                                 <td>
-                                    {{ $bitacora->fecha_hora}}
-                                </td>           
+                                    {{ $bitacora->fecha_hora }}
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
