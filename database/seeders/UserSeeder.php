50564d22a2497227a1e50285582f80f5154e933a
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2023-07-21 14:05:16 -0400

    seeders de users actualizado con los roles asignados

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,24 +13,24 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         $usuarios = [
             //nro 1
             [
                 'name' => 'Eduardo Rojas Calderon',
-                'email' => 'calderoneduardo123sc@gmail.com',
+                'email' => 'eduardo123sc@gmail.com',
                 'password' => bcrypt('87654321')
             ],
             //nro 2
             [
-                'name' => 'Alejandro',
-                'email' => 'alejandrol123@gmail.com',
+                'name' => 'Antonio Quinteros',
+                'email' => 'antonio123@gmail.com',
                 'password' => bcrypt('password123')
             ],
             //nro 3
             [
-                'name' => 'Jhonn Montoya',
-                'email' => 'montoya@gmail.com',
+                'name' => 'Pepe Rosado',
+                'email' => 'pepe123sc@gmail.com',
                 'password' => bcrypt('password123')
             ],
             //nro 4
@@ -51,7 +51,7 @@ class UserSeeder extends Seeder
                 'email' => 'rolando123@gmail.com',
                 'password' => bcrypt('password123')
             ],
-           //nro 7
+            //nro 7
             [
                 'name' => 'María González',
                 'email' => 'maria@hotmail.com',
@@ -64,21 +64,45 @@ class UserSeeder extends Seeder
                 'password' => bcrypt('password123')
             ],
         ];
-    
+
         foreach ($usuarios as $usuario) {
             User::create($usuario);
         }
 
-        User::create([
-            'name'=>'Juan Pablo Rodriguez',
-            'email'=>'pablojuan123sc@gmail.com',
-            'password'=>bcrypt(12345678)
-        ])->assignRole('Administrador');
+        //ADMINISTRADORES POR DEFECTO
 
         User::create([
-            'name'=>'Pedro Fernandez',
-            'email'=>'pedrofer123sc@gmail.com',
-            'password'=>bcrypt(12345678)
+            'name' => 'Juan Pablo Rodriguez',
+            'email' => 'pablojuan123sc@gmail.com',
+            'password' => bcrypt(12345678)
+        ])->assignRole('Administrador');
+        User::create([
+            'name' => 'Carlos Benjamin Romero',
+            'email' => 'carlos123sc@gmail.com',
+            'password' => bcrypt(12345678)
+        ])->assignRole('Administrador');
+        User::create([
+            'name' => 'Camilo Sarmiento',
+            'email' => 'camilo123sc@gmail.com',
+            'password' => bcrypt(12345678)
+        ])->assignRole('Administrador');
+        User::create([
+            'name' => 'Alejandro Calzadilla',
+            'email' => 'alejandro123sc@gmail.com',
+            'password' => bcrypt(12345678)
+        ])->assignRole('Administrador');
+
+        //EJECUTIVOS DE VENTAS(VENDEDORES)
+
+        User::create([
+            'name' => 'Pedro Fernandez',
+            'email' => 'pedrofer123sc@gmail.com',
+            'password' => bcrypt(12345678)
+        ])->assignRole('Ejecutivo de ventas');
+        User::create([
+            'name' => 'Diego Robles',
+            'email' => 'diego123sc@gmail.com',
+            'password' => bcrypt(12345678)
         ])->assignRole('Ejecutivo de ventas');
     }
 }
